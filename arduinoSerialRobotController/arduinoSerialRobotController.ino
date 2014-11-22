29967edf51a662d5a14a13a8e549bc1154534c15
Neal Kerry <nealkerry@Neals-MBP.dhcp.lan.london.hackspace.org.uk>
2014-11-22 11:50:32 +0000

    Updated controller for variable speed

--- arduinoSerialRobotController/arduinoSerialRobotController.ino
+++ arduinoSerialRobotController/arduinoSerialRobotController.ino
@@ -4,8 +4,12 @@
 // and instead of digitalWrite, you should use the analogWrite command
 
 // --------------------------------------------------------------------------- Motors
-int motor_left[] = {2, 3};
-int motor_right[] = {7, 8};
+int FORWARD = 0;
+int BACK = 1;
+byte STOP = 0;
+byte FORWARD_THRESH = 127;
+int motor_left[] = {5, 6};
+int motor_right[] = {9, 10};
 
 // --------------------------------------------------------------------------- Setup
 void setup() {
@@ -16,6 +20,7 @@ void setup() {
     pinMode(motor_left[i], OUTPUT);
     pinMode(motor_right[i], OUTPUT);
   }
+  motor_stop();
 }
 
 // --------------------------------------------------------------------------- Loop
@@ -23,65 +28,79 @@ void loop() {
 }
 
 void serialEvent() {
-  if (Serial.available()==1) {
-    char motor = (char)Serial.read();
+  if (Serial.available()==2) {
+    byte leftPower = Serial.read();
+    byte rightPower = Serial.read();
+    
+    turn_left(leftPower);
+    turn_right(rightPower);
 
-    if(motor == 'f') {
-      drive_forward();
-    }
-    if(motor == 'b') {
-      drive_backward();
-    }
-    if(motor == 'l') {
-      turn_left();
-    }
-    if(motor == 'r') {
-      turn_right();
-    }
-    if(motor == 's') {
-      motor_stop();
-    }
+//    if(motor == 'f') {
+//      drive_forward();
+//    }
+//    if(motor == 'b') {
+//      drive_backward();
+//    }
+//    if(motor == 'l') {
+//      turn_left();
+//    }
+//    if(motor == 'r') {
+//      turn_right();
+//    }
+//    if(motor == 's') {
+//      motor_stop();
+//    }
   }
 }
 
 // --------------------------------------------------------------------------- Drive
 
 void motor_stop(){
-  digitalWrite(motor_left[0], LOW); 
-  digitalWrite(motor_left[1], LOW); 
+  analogWrite(motor_left[0], STOP); 
+  analogWrite(motor_left[1], STOP); 
   
-  digitalWrite(motor_right[0], LOW); 
-  digitalWrite(motor_right[1], LOW);
+  analogWrite(motor_right[0], STOP); 
+  analogWrite(motor_right[1], STOP);
 }
 
 void drive_forward(){
-  digitalWrite(motor_left[0], HIGH); 
-  digitalWrite(motor_left[1], LOW); 
+  analogWrite(motor_left[0], HIGH); 
+  analogWrite(motor_left[1], LOW); 
   
-  digitalWrite(motor_right[0], HIGH); 
-  digitalWrite(motor_right[1], LOW); 
+  analogWrite(motor_right[0], HIGH); 
+  analogWrite(motor_right[1], LOW); 
 }
 
 void drive_backward(){
-  digitalWrite(motor_left[0], LOW); 
-  digitalWrite(motor_left[1], HIGH); 
+  analogWrite(motor_left[0], LOW); 
+  analogWrite(motor_left[1], HIGH); 
 
-  digitalWrite(motor_right[0], LOW); 
-  digitalWrite(motor_right[1], HIGH); 
+  analogWrite(motor_right[0], LOW); 
+  analogWrite(motor_right[1], HIGH); 
 }
 
-void turn_left(){
-  digitalWrite(motor_left[0], LOW); 
-  digitalWrite(motor_left[1], HIGH); 
-  
-  digitalWrite(motor_right[0], HIGH); 
-  digitalWrite(motor_right[1], LOW);
+void turn_left(byte power){
+  if(power > FORWARD_THRESH){
+    power = 2 * (power - 127);
+    analogWrite(motor_left[BACK], STOP); 
+    analogWrite(motor_left[FORWARD], power); 
+  }
+  else{
+    power = 2 * (power);
+    analogWrite(motor_left[FORWARD], STOP); 
+    analogWrite(motor_left[BACK], power + FORWARD_THRESH); 
+  }
 }
 
-void turn_right(){
-  digitalWrite(motor_left[0], HIGH); 
-  digitalWrite(motor_left[1], LOW); 
-  
-  digitalWrite(motor_right[0], LOW); 
-  digitalWrite(motor_right[1], HIGH); 
+void turn_right(byte power){
+  if(power > FORWARD_THRESH){
+    power = 2 * (power - 127);
+    analogWrite(motor_right[BACK], STOP); 
+    analogWrite(motor_right[FORWARD], power); 
+  }
+  else{
+    power = 2 * (power);
+    analogWrite(motor_right[FORWARD], STOP); 
+    analogWrite(motor_right[BACK], power + FORWARD_THRESH); 
+  }
 }
